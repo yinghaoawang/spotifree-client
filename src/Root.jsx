ecf574a4c6512be52fe265d817303b11670acfab
Yinghao Alan Wang <yinghao.a.wang@gmail.com>
2024-07-22 13:53:33 -0700

    add media tray and youtube player

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './Root.css';
+import MediaTray from './components/MediaTray';
 
 export default function Root() {
   return (
-    <>
+    <div id='main'>
       <Navbar />
       <div id='detail'>
         <Outlet />
       </div>
-    </>
+      <MediaTray />
+    </div>
   );
 }
